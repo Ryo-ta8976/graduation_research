2e995ffc887a4c38921424878952288bff83fe1c
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-08-28 11:34:51 +0900

    ラズパイデータを取得してjsonに変換

--- webServer.js
+++ webServer.js
@@ -4,15 +4,24 @@ var url = require('url');
 var fs = require('fs');
 var server = http.createServer();
 
+
+//DB接続準備
 const MongoClient = require('mongodb').MongoClient;
-// Connection URL
- const url_db = 'mongodb://localhost:27017';
-// Database Name
-const dbName = 'study';
+ const url_db = 'mongodb://localhost:27017'; // Connection URL
+const dbName = 'study'; // Database Name
 const client = new MongoClient(url_db,{useNewUrlParser: true});
 const assert = require('assert');
 var collection;
 var value;
+
+
+//rasberrypiとの接続準備
+var https = require('https');
+var url_json = 'https://api.atnd.org/events/?keyword_or=javascript&format=json';
+var data_raspi = [];
+var data_string = "";
+
+
 
 // Use connect method to connect to the server
 client.connect(function(err) {
@@ -77,37 +86,57 @@ server.on('request', function (req, res) {
              });
           },
           "getValue": function () {
-            //結果
-           // var value;
+                //結果
+                // var value;
+                    
+                // Find some documents
+                /*collection.find({}).toArray(function(err, docs) {
+                    //assert.equal(err, null);
+                    //console.log("Found the following records");
+                    value=docs;
+                    console.log(value);
+                    callback(docs);
+                });*/
+                /*value=collection.find();
+                console.log(value);*/
                 
-            // Find some documents
-            /*collection.find({}).toArray(function(err, docs) {
-                //assert.equal(err, null);
-                //console.log("Found the following records");
-                value=docs;
+                client.close();
+                
+                // HTTPレスポンスヘッダを出力する
+                res.writeHead(200, {
+                    'content-Type': 'text/html',
+                    'Access-Control-Allow-Origin': '*'
+                });
+
+                console.log("レスポンス");
+                //console.log(result);
+
                 console.log(value);
-                callback(docs);
-            });*/
-            /*value=collection.find();
-            console.log(value);*/
-            
-            client.close();
-            
-            // HTTPレスポンスヘッダを出力する
-            res.writeHead(200, {
-                'content-Type': 'text/html',
-                'Access-Control-Allow-Origin': '*'
-            });
-
-            console.log("レスポンス");
-            //console.log(result);
-
-            console.log(value);
-            // HTTPレスポンスボディを出力する
-            res.write(JSON.stringify(value));
-            res.end();
+                // HTTPレスポンスボディを出力する
+                res.write(JSON.stringify(value));
+                res.end();
+           },
+           "postData": function() {
+                req.on('data', function(data) {
+                    data_string += data.toString('utf-8', 0, data.length); //バッファをstringに変換
+                    //data_json = JSON.parse(data_string);
+                    //console.log(typeof(data_json));
+                   // var buf = Buffer.from(JSON.stringify(data));
+                    //var temp =JSON.parse(buf.toString);
+                    //console.log(temp);
+                    //console.log(data.toJSON());
+                    //data_parse = JSON.parse(data);
+                    //console.log(data_parse);
+                    //data_raspi.push(data.toJSON());
+                    //data_json = JSON.parse(data || "null");
+                });
+                req.on('end', function() {
+                    //console.log(data_raspi);
+                    var data_json = JSON.parse(data_string);
+                    console.log(data_json.rot);
+                });
            }
-  };
+    };
     // urlのpathをuriに代入
     var uri = url.parse(req.url).pathname;
     console.log(uri);
@@ -115,24 +144,27 @@ server.on('request', function (req, res) {
 
     // URIで行う処理を分岐させる
     if (uri === "/") {
-        // URLが「http://localhost:8080/」の場合、"renderHTML"の処理を行う
+        // URLが「IPアドレス/:1234/」の場合、"renderHTML"の処理を行う
         Response["renderHTML"]();
         return;
     } else if (uri === "/js/three.js-master/build/three.min.js") {
-        // URLが「http://localhost:8080/js/three.js-master/build/three.min.js」の場合、"getThree"の処理を行う
+        // URLが「IPアドレス/:1234/js/three.js-master/build/three.min.js」の場合、"getThree"の処理を行う
         Response["getThree"]();
         return;
     } else if (uri === "/js/OrbitControls.js") {
-        // URLが「http://localhost:8080/js/OrbitControls.js」の場合、"getOrbit"の処理を行う
+        // URLが「IPアドレス/:1234/js/OrbitControls.js」の場合、"getOrbit"の処理を行う
         Response["getOrbit"]();
         return;
     } else if (uri === "/get_value") {
-        // URLが「http://localhost:8080/get_value」の場合、"getThree"の処理を行う
+        // URLが「IPアドレス/:1234/get_value」の場合、"getThree"の処理を行う
         Response["getValue"]();
         return;
+    } else if (uri === "/post_data") {
+        // URLが「IPアドレス/:1234/post_data」の場合、"postData"の処理を行う
+        Response["postData"]();
     };
   });
 
-  // 指定されたポート(8080)でコネクションの受け入れを開始する
+  // 指定されたポート(1234)でコネクションの受け入れを開始する
   server.listen(1234);
-  console.log('Server running at http://localhost:80/');
+  console.log('Server running ');
