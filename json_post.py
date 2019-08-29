6b0b6439ce1e4620f0cce48afdf399612bd9c4c1
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-08-29 15:35:19 +0900

    微調整

--- json_post.py
+++ json_post.py
@@ -4,8 +4,8 @@ import json
 
 
 with open('test2.json') as file:
-  data = json.load(file)
-  data = json.dumps(data) #stringに変換
+    data = json.load(file)
+    data = json.dumps(data)  # stringに変換
 
 url = 'http://192.168.50.226:1234/post_data'
 #url = 'http://172.16.10.137:1234/post_data'
