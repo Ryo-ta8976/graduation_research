1f2f8f06fd0e074b9f08bd90e10c32f893013836
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-08-29 13:33:45 +0900

    取得した情報をjsonで送信する処理を追加

--- raspi/lidar_gyro_new.py
+++ raspi/lidar_gyro_new.py
@@ -1,15 +1,16 @@
 import serial
 import smbus            # use I2C
 import math             # mathmatics
-import  time
+import time
 from time import sleep  # time module
 import numpy as np
 import json
 import collections as cl
+import requests
 
 
 # slave address
-t_before=time.time()
+t_before = time.time()
 
 DEV_ADDR = 0x68         # device address
 # register address
@@ -21,18 +22,18 @@ GYRO_XOUT = 0x43
 GYRO_YOUT = 0x45
 GYRO_ZOUT = 0x47
 
-address = 0x62  #Lider Lite v3のアドレス
+address = 0x62  # Lider Lite v3のアドレス
 ACQ_COMMAND = 0x00
 STATUS = 0x01
 FULL_DELAY_HIGH = 0x0f
 FULL_DELAY_LOW = 0x10
 
-sum_degree=0.0
-timeval=0.001
-i=0
-gyro_z=[]
-rotation_degree=[]
-dist=[]
+sum_degree = 0.0
+timeval = 0.001
+i = 0
+gyro_z = []
+rotation_degree = []
+dist = []
 
 bus = smbus.SMBus(1)
 #bus.write_byte_data(DEV_ADDR, PWR_MGMT_1, 0)
@@ -42,12 +43,16 @@ bus = smbus.SMBus(1)
 def read_byte(adr):
     return bus.read_byte_data(DEV_ADDR, adr)
 # 2byte read
+
+
 def read_word(adr):
     high = bus.read_byte_data(DEV_ADDR, adr)
     low = bus.read_byte_data(DEV_ADDR, adr+1)
     val = (high << 8) + low
     return val
 # Sensor data read
+
+
 def read_word_sensor(adr):
     val = read_word(adr)
     if (val >= 0x8000):         # minus
@@ -55,42 +60,44 @@ def read_word_sensor(adr):
     else:                       # plus
         return val
 
+
 def get_gyro_data_lsb():
     z = read_word_sensor(GYRO_ZOUT)
     return z
 
+
 def get_gyro_data_deg():
-    z=get_gyro_data_lsb()
+    z = get_gyro_data_lsb()
     z = z / 131.0
     return z
 
 
 def get_dist():
-    #0x00に0x04の内容を書き込む
-    bus.write_block_data(address,ACQ_COMMAND,[0x04])
+    # 0x00に0x04の内容を書き込む
+    bus.write_block_data(address, ACQ_COMMAND, [0x04])
 
-    #0x01を読み込んで、最下位bitが0になるまで読み込む
+    # 0x01を読み込んで、最下位bitが0になるまで読み込む
     value = bus.read_byte_data(address, STATUS)
-    while value & 0x01==1:
+    while value & 0x01 == 1:
         value = bus.read_byte_data(address, STATUS)
 
-    #0x8fから2バイト読み込んで16bitの測定距離をcm単位で取得する
+    # 0x8fから2バイト読み込んで16bitの測定距離をcm単位で取得する
 
     high = bus.read_byte_data(address, FULL_DELAY_HIGH)
-    low = bus.read_byte_data(address,FULL_DELAY_LOW)
+    low = bus.read_byte_data(address, FULL_DELAY_LOW)
     val = (high << 8) + low
     dist = val
-    
+
     return dist
 
 
 for i in range(2000):
-    value=get_dist()
+    value = get_dist()
 
-    z=get_gyro_data_deg()
+    z = get_gyro_data_deg()
     t_after = time.time()
-    elapsed_time=t_after-t_before
-    t_before=t_after
+    elapsed_time = t_after-t_before
+    t_before = t_after
     gyro_z.append(z)
 
     dist.append(value)
@@ -98,29 +105,32 @@ for i in range(2000):
 
     sleep(timeval)
 
- 
-#file=open('test_lidar_time.csv','w')
 
-sum=0
-rot=[]
+# file=open('test_lidar_time.csv','w')
+
+sum = 0
+rot = []
 for i in range(2000):
-    sum_degree=sum_degree+rotation_degree[i]
+    sum_degree = sum_degree+rotation_degree[i]
     rot.append(sum_degree)
     #file.write("%d,%08.3f\n" % (dist[i],sum_degree) )
     print('No. %d' % i)
-    print("distance: %d" % dist[i] )
+    print("distance: %d" % dist[i])
     print('degree: %08.3f' % sum_degree)
     print('\n')
 
-ys=cl.OrderedDict()
-data=cl.OrderedDict()
-data["dist"]=dist
-data["rot"]=rot
-ys=data
+# ys=cl.OrderedDict()
+data = cl.OrderedDict()
+data["dist"] = dist
+data["rot"] = rot
+data = json.dumps(data)  # objectからstringに変換
+url = 'http://192.168.50.226:1234/post_data'
+#url = 'http://172.16.10.137:1234/post_data'
+result = requests.post(url, data)
+# ys=data
 
 
+# fw=open('test.json','w')
+# json.dump(ys,fw,indent=3)
 
-fw=open('testttt.json','w')
-json.dump(ys,fw,indent=3)
-
-#file.close()
+# file.close()
