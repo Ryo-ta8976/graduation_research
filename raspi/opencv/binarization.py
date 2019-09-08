93f662f4ae09b628483c3b6ba0754abf5a770470
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-09-08 16:54:52 +0900

    picture size change

--- raspi/opencv/binarization.py
+++ raspi/opencv/binarization.py
@@ -6,7 +6,11 @@ if __name__ == '__main__':
     
     temp_image_path = './image_1.jpg'
     gray_temp_src = cv2.imread(temp_image_path, cv2.IMREAD_GRAYSCALE)
-    ret,img_thresh = cv2.threshold(gray_temp_src, 10, 255, cv2.THRESH_BINARY_INV)
+    ret,img_thresh = cv2.threshold(gray_temp_src, 70, 255, cv2.THRESH_BINARY_INV)
+    height,width = img_thresh.shape[:2]
+    img_thresh = cv2.resize(img_thresh, dsize=None, fx=0.2, fy=0.2)
     cv2.imshow("img_thresh", img_thresh)
     cv2.waitKey()
     cv2.destroyAllWindows()
+    
+    
