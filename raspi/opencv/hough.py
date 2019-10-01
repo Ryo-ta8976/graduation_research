00a3527361624bc9c0a03d7e6e0341db64adf479
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-10-01 13:44:33 +0900

    hough.py change

--- raspi/opencv/hough.py
+++ raspi/opencv/hough.py
@@ -1,15 +1,35 @@
 import cv2
+import numpy as np
+import math
 
-img = cv2.imread("sample.jpg")
+img = cv2.imread("sample_hough.png")
 
 # グレースケールに変換する。
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Canny 法で2値化する。
-edges = cv2.Canny(gray, 150, 300, L2gradient=True)
-imshow(edges)
+#edges = cv2.Canny(gray, 10, , L2gradient=True)
+#cv2.imshow('result_hough.png',edges)
+ret, edges = cv2.threshold(gray,10,250,cv2.THRESH_BINARY)
+
+pi = np.pi
 
 # ハフ変換で直線検出する。
-lines = cv2.HoughLines(edges, 1, np.pi / 180, 100)
-lines = lines.squeeze(axis=1)
-print(lines)
+lines = cv2.HoughLines(edges, 1, pi / 180, 200)
+for line in lines:
+    for rho, theta in line:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+        
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+        radian = math.atan2(y2-y1, x2-x1)
+        radian = math.degrees(pi/radian)
+        print("radian:"+ str(radian))
+
+cv2.imwrite('result_hough.png', img)
