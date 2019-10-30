470291f28984d901ec029cb2f6f5b7040ab66115
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-10-30 12:59:45 +0900

    gas data to server

--- raspi/sensor/gas/get_gas_http.py
+++ raspi/sensor/gas/get_gas_http.py
@@ -1,9 +1,14 @@
 import serial
 import time
+import collections as cl
+import re
 
+def get_num(str):
+    result=re.findall("\d+\.\d+", str)
+    return result
 
 def main():
-    ser = serial.Serial('/dev/ttyUSB0', 9600)
+    ser = serial.Serial('/dev/ttyUSB1', 9600)
     time.sleep(2)
     t0 = time.time()
 
@@ -11,7 +16,15 @@ def main():
         # ser.write("114514")
         str = ser.readline()
         val = (str.decode())
+        gas_list=get_num(val)
         print(val)
+        data = cl.OrderedDict()
+        data["gas"] = gas_list
+        data = json.dumps(data)  # objectからstringに変換
+        url = 'http://192.168.10.3:1234/post_gas'
+        #url = 'http://172.16.10.137:1234/post_data'
+        result = requests.post(url, data)
+        time.sleep(60)
 
 
 if __name__ == '__main__':
