8839fa5fc64a28c53c6c7eb9a221dcc9f5672dd4
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-11-17 11:23:14 +0900

    drone.connectを外にだす

--- raspi/bebop/measure.js
+++ raspi/bebop/measure.js
@@ -2,70 +2,74 @@ var bebop = require('node-bebop');
 
 var drone = bebop.createClient();
 
-//setTimeoutをいちいち書くのが面倒なので関数化
-function Timeout(passVal, ms) {
-  return new Promise(resolve =>
-    setTimeout(() => {
-      resolve(passVal);
-    }, ms)
-  )
-}
+drone.connect(function () {
 
-async function f1() {
-  drone.connect(function () {
+  //setTimeoutをいちいち書くのが面倒なので関数化
+  function Timeout(passVal, ms) {
+    return new Promise(resolve =>
+      setTimeout(() => {
+        resolve(passVal);
+      }, ms)
+    )
+  }
+
+  async function f1() {
+
     drone.takeOff();
 
     setTimeout(function () {
       drone.stop();
     }, 3000);
-  });
-  return "drone take off";
-}
-async function f2(passVal) {
-  drone.connect(function () {
+
+    return "drone take off";
+  }
+  async function f2(passVal) {
+
     drone.up(50);
 
     setTimeout(function () {
       drone.stop();
     }, 3000);
-  });
-  return "drone up";
-}
 
-async function f3(passVal) {
-  const execSync = require('child_process').execSync;
-  const result = execSync('raspistill -o linear.jpg');
-  return "take a picture";
-}
-
-async function f4(passVal) {
-  console.log("#4: f4");
-  return Timeout("f4", Math.random() * 3000);
-
-}
-
-
-async function runAll() {
-  try {
-    console.log("[START]");
-    const res1 = await f1();
-    console.log(res1);
-
-    const res2 = await f2(res1);
-    console.log(res2);
-
-    const res3 = await f3(res2);
-    console.log(res3);
-
-    const res4 = await f4(res3);
-    console.log("Final function: " + res4);
-
-    console.log("[END]");
-
-  } catch (err) {
-    //エラー処理
-    //とりあえず何もしない
+    return "drone up";
   }
-}
 
-runAll();
+  async function f3(passVal) {
+    const execSync = require('child_process').execSync;
+    const result = execSync('raspistill -o linear.jpg');
+    return "take a picture";
+  }
+
+  async function f4(passVal) {
+    console.log("#4: f4");
+    return Timeout("f4", Math.random() * 3000);
+
+  }
+
+
+  async function runAll() {
+    try {
+      console.log("[START]");
+      const res1 = await f1();
+      console.log(res1);
+
+      const res2 = await f2(res1);
+      console.log(res2);
+
+      const res3 = await f3(res2);
+      console.log(res3);
+
+      const res4 = await f4(res3);
+      console.log("Final function: " + res4);
+
+      console.log("[END]");
+
+    } catch (err) {
+      //エラー処理
+      //とりあえず何もしない
+    }
+  }
+
+  runAll();
+
+});
