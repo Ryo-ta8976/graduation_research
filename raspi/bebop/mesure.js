54bcfe40d9baa2f23154cbe0c5f476b14e7b2a0c
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-12-10 21:15:00 +0900

    mesure.js change

--- raspi/bebop/mesure.js
+++ raspi/bebop/mesure.js
@@ -1,4 +1,6 @@
-const startTime_all = perfomance.now();
+const performance = require('perf_hooks').performance;
+
+const startTime_all = performance.now();
 var bebop = require('node-bebop');
 var { PythonShell } = require('python-shell');
 
@@ -30,21 +32,21 @@ drone.connect(function () {
 
   //カメラ撮影
   setTimeout(function () {
-    startTime = perfomance.now();
+    startTime = performance.now();
     const execSync = require('child_process').execSync;
     const result = execSync('raspistill -o linear.jpg');
-    endTime = perfomance.now();
+    endTime = performance.now();
     console.log("take a picture: %f", endTime - startTime);
   }, 15000);
 
   //Hough変換
   setTimeout(function () {
-    startTime = perfomance.now();
+    startTime = performance.now();
     var pyshell = new PythonShell('../opencv/hough.py');
     pyshell.on('message', function (data) {
       console.log(data);
       mesure_rotation_degree_1 = data;
-      endTime = perfomance.now();
+      endTime = performance.now();
       console.log("hough did: %f", endTime - startTime);
     });
   }, 20000);
@@ -54,12 +56,12 @@ drone.connect(function () {
     drone.counterClockwise(100);
     console.log("turning");
 
-    startTime = perfomance.now();
+    startTime = performance.now();
     var pyshell_layout = new PythonShell('../sensor/lidar_gyro_new.py');
     pyshell_layout.on('message', function (data) {
       if (data == "stop") {
         drone.stop();
-        endTime = perfomance.now();
+        endTime = performance.now();
         console.log("stop now: %f", endTime - startTime);
         //drone.land();
       } else {
@@ -83,27 +85,32 @@ drone.connect(function () {
     pyshell.on('message', function (data) {
       console.log(data);
       mesure_rotation_degree_2 = data;
+	console.log("hogh did");
     });
-    console.log("hough did");
+    
   }, 40000);
 
   //角度補正計算
   setTimeout(function () {
-    startTime = perfomance.now();
+    startTime = performance.now();
     var error_degree = mesure_rotation_degree_1 - mesure_rotation_degree_2;
-    pyshell_layout.send(error_degree);
-    endTime = perfomance.now();
+    pyshell.send(error_degree);
+    endTime = performance.now();
+
+	pyshell.on('message', function (data) {
+      console.log(data);
+    });
 
     console.log("error_degree send: %f", endTime - startTime);
   }, 45000);
 
   setTimeout(function () {
     drone.stop();
-  }, 35000);
+  }, 50000);
 
   setTimeout(function () {
     drone.land();
-    const endTime_all = perfomance.now();
+    const endTime_all = performance.now();
     console.log("end: %f", endTime_all - startTime_all);
-  }, 40000);
+  }, 55000);
 });
