ffd5aab3eb2cd387ed32b7fdb15e3895986fb62e
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2019-12-17 15:31:28 +0900

    全体ループ追加

--- raspi/bebop/move.js
+++ raspi/bebop/move.js
@@ -34,81 +34,88 @@ drone.connect(function () {
     resolve('end mesure_rssi')
   })
 
-  mesure_rssi.then((msg) => { // 回転させる角度の計算
-    console.log(msg)
+  while (1) {
+    mesure_rssi.then((msg) => { // 回転させる角度の計算
+      console.log(msg)
 
-    return new Promise((resolve) => {
-      var temp_array = [];
+      return new Promise((resolve) => {
+        var temp_array = [];
 
-      //昇順ソート
-      temp_array.sort(
-        function (a, b) {
-          return (a < b ? -1 : 1);
+        temp_array = rssi_array;
+        //昇順ソート
+        temp_array.sort(
+          function (a, b) {
+            return (a < b ? -1 : 1);
+          }
+        );
+
+        let rssi_max = temp_array[9];
+        let index_array = rssi_array.indexOf(rssi_max); //電波強度が最大の時のindex
+        let rotate_bebop; //bebopを回転させる角度index
+
+        if (index_array == 0) {
+          rotate_bebop = 6;
+        } else if (index_array == 1) {
+          rotate_bebop = 7;
+        } else {
+          rotate_bebop = index_array - 2;
         }
-      );
 
-      let rssi_max = temp_array[9];
-      let index_array = rssi_array.indexOf(rssi_max); //電波強度が最大の時のindex
-      let rotate_bebop; //bebopを回転させる角度index
+        resolve(rotate_bebop);
+      })
+    }).then((rotate_bebop) => { // bebopの回転
+      return new Promise((resolve) => {
+        let rotate_time = 1;
 
-      if (index_array == 0) {
-        rotate_bebop = 6;
-      } else if (index_array == 1) {
-        rotate_bebop = 7;
-      } else {
-        rotate_bebop = index_array - 2;
-      }
+        drone.counterClockwise(100);
+        console.log("turning");
 
-      resolve(rotate_bebop);
+        setTimeout(() => {
+          drone.stop();
+          resolve("drone stop")
+        }, rotate_bebop * rotate_time);
+      })
+    }).then((msg) => { // bebopの直進
+      console.log(msg)
+
+      return new Promise((resolve) => {
+        let forwarding_time = 2;
+
+        drone.forward(100);
+        console.log("forwarding");
+
+        setTimeout(() => {
+          drone.stop();
+          resolve("drone stop")
+        }, forwarding_time);
+      })
+    }).then((msg) => { //真下方向の撮影
+      console.log(msg)
+
+      return new Promise((resolve) => {
+        const execSync = require('child_process').execSync;
+        const result = execSync('raspistill -o linear.jpg');
+        resolve("take a picture")
+      })
+    }).then((msg) => {
+      console.log(msg)
+
+      var pyshell = new PythonShell('../opencv/hough.py');
+      pyshell.on('message', function (data) {
+        if (data == "error") {
+          console.log("continue...")
+        } else {
+          console.log("end")
+          drone.land();
+
+          break;
+        }
+      });
+
+    }).catch(() => { // エラーハンドリング
+      console.error('Something wrong!')
     })
-  }).then((rotate_bebop) => { // bebopの回転
-    return new Promise((resolve) => {
-      let rotate_time = 1;
+  }
 
-      drone.counterClockwise(100);
-      console.log("turning");
 
-      setTimeout(() => {
-        drone.stop();
-        resolve("drone stop")
-      }, rotate_bebop * rotate_time);
-    })
-  }).then((msg) => { // bebopの直進
-    console.log(msg)
-
-    return new Promise((resolve) => {
-      let forwarding_time = 2;
-
-      drone.forward(100);
-      console.log("forwarding");
-
-      setTimeout(() => {
-        drone.stop();
-        resolve("drone stop")
-      }, forwarding_time);
-    })
-  }).then((msg) => { //真下方向の撮影
-    console.log(msg)
-
-    return new Promise((resolve) => {
-      const execSync = require('child_process').execSync;
-      const result = execSync('raspistill -o linear.jpg');
-      resolve("take a picture")
-    })
-  }).then((msg) => {
-    console.log(msg)
-
-    var pyshell = new PythonShell('../opencv/hough.py');
-    pyshell.on('message', function (data) {
-      if (data == "error") {
-        ////////////////////////////////
-      } else {
-        drone.land();
-        return
-      }
-    });
-
-  }).catch(() => { // エラーハンドリング
-    console.error('Something wrong!')
-  })
 });
