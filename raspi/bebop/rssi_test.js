a77fc93eccaf81118c02df5cc6b7455aba43ebb2
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2020-01-14 09:30:15 +0900

    関数に変更

--- raspi/bebop/rssi_test.js
+++ raspi/bebop/rssi_test.js
@@ -1,6 +1,5 @@
 var noble = require('noble');
 var fs = require('fs');
-//const csvSync = require('csv-parse/lib/sync');
 
 var DEVICE_NAME = "ble_koji";
 var SERVICE_UUID = "713d0000503e4c75ba943148f18d941e";
@@ -10,67 +9,53 @@ var count = 0;
 var rssi_array = [];
 let array = [];
 
-const get_rssi = new Promise(resolve => {
-  console.log("rssi mesuring...")
-  // var temp = rssi.ave
-  // import('../rssi/rssi.js').then((rssi_ave) => {
-  //   resolve(rssi_ave)
-  // })
+function get_rssi() {
 
-  //start ble
-  noble.on('stateChange', function (state) {
-    if (state === 'poweredOn') {
-      noble.startScanning();
-    } else {
-      noble.stopScanning();
-    }
-  });
+  return new Promise(resolve => {
+    console.log("rssi mesuring...")
 
-  //search ble
-  noble.on('discover', function (peripheral) {
-    //output seach device
-    console.log("DEVICE_NAME: " + peripheral.advertisement.localName);
-    console.log("UUID: " + peripheral.uuid);
-    console.log("RSSI: " + peripheral.rssi);
-    console.log();
-
-    //equals devicename
-    if (peripheral.advertisement.localName == DEVICE_NAME) {
-      count++;
-      console.log("device find");
-      noble.stopScanning();
-
-      array.push(peripheral.rssi);
-      // fs.appendFileSync('/home/pi/Desktop/kenkyu/raspi/rssi/ble.csv', peripheral.rssi + ',', (error) => {
-      // });
-      if (count < 10) {
+    //start ble
+    noble.on('stateChange', function (state) {
+      if (state === 'poweredOn') {
         noble.startScanning();
       } else {
-        //let data = fs.readFileSync('/home/pi/Desktop/kenkyu/raspi/rssi/ble.csv');
-        //let res = csvSync(data);    
-
-        //array = res[0];
-        //array = array.map(Number);
-        console.log(array);
-        array.sort(
-          function (a, b) {
-            return (a < b ? -1 : 1);
-          }
-        );
-
-        // let ave = array[9];
-        // exports.ave = ave;
-        // console.log(ave);
-        // fs.appendFileSync('/home/pi/Desktop/kenkyu/raspi/rssi/ble_ave.csv', ave + ',', (error) => {
-        // });
-        //
-        console.log(array[9]);
-        resolve(array[9]);
-        //fs.unlinkSync('/home/pi/Desktop/kenkyu/raspi/rssi/ble.csv');
+        noble.stopScanning();
       }
-    }
-  })
-});
+    });
+
+    //search ble
+    noble.on('discover', function (peripheral) {
+      //output seach device
+      console.log("DEVICE_NAME: " + peripheral.advertisement.localName);
+      console.log("UUID: " + peripheral.uuid);
+      console.log("RSSI: " + peripheral.rssi);
+      console.log();
+
+      //equals devicename
+      if (peripheral.advertisement.localName == DEVICE_NAME) {
+        count++;
+        console.log("device find");
+        noble.stopScanning();
+
+        array.push(peripheral.rssi);
+
+        if (count < 10) {
+          noble.startScanning();
+        } else {
+          console.log(array);
+          array.sort(
+            function (a, b) {
+              return (a < b ? -1 : 1);
+            }
+          );
+
+          console.log(array[9]);
+          resolve(array[9]);
+        }
+      }
+    })
+  });
+}
 
 
 function sleep(waitSec) {
@@ -86,8 +71,11 @@ const main = async () => {
   console.log("aaa");
 
   for (let i = 0; i < 8; i++) {
-    let rssi_max = await get_rssi;
+    let rssi_max = await get_rssi();
     rssi_array.push(rssi_max);
+    // get_rssi.then((rssi_max) => {
+    //   rssi_array.push(rssi_max);
+    // })
     console.log(rssi_max)
     console.log(i);
 
