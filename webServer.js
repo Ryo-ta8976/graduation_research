5d182ead4ea752f5d9aeeb4a344324168473380c
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2020-04-22 11:32:39 +0900

    JSDocに変更

--- webServer.js
+++ webServer.js
@@ -1,270 +1,293 @@
-// 必要なファイルを読み込み
+/**
+ * http通信用モジュールの読み込み
+ * @type {Object} 
+ */
 var http = require('http');
+/**
+ * http通信のurl取得用モジュールの読み込み
+ * @type {Object} 
+ */
 var url = require('url');
+/**
+ * ファイル読み込み用モジュールの読み込み
+ * @type {Object} 
+ */
 var fs = require('fs');
+
+
+/**
+ * httpサーバの初期化
+ * @type {Object} 
+ */
 var server = http.createServer();
 
 
-//DB接続準備
+/**
+ * mongodbライブラリの読み込み
+ * @type {Object} 
+ */
 const MongoClient = require('mongodb').MongoClient;
-const url_db = 'mongodb://localhost:27017'; // Connection URL
-const dbName = 'study'; // Database Name
-const client = new MongoClient(url_db, { useNewUrlParser: true });
-const assert = require('assert');
-var collection;
-//var value;
+/**
+ * mongodbの接続先URL
+ * @type {String} 
+ */
+const URL_DB = 'mongodb://localhost:27017';
+/**
+ * mongodbの接続先データベース名
+ * @type {String} 
+ */
+const DB_NAME = 'study';
+/**
+ * mongodbのインスタンスの作成
+ * @type {Object} 
+ */
+var client = new MongoClient(URL_DB, { useNewUrlParser: true });
+/**
+ * mongodbのコレクションの初期化
+ * @type {Object} 
+ */
+var collection = {};
 
 
 //rasberrypiとの接続準備
-var https = require('https');
-var url_json = 'https://api.atnd.org/events/?keyword_or=javascript&format=json';
-var data_raspi = [];
+//var https = require('https');
+//var url_json = 'https://api.atnd.org/events/?keyword_or=javascript&format=json';
+//var data_raspi = [];
+/**
+ * データ受信用変数の初期化
+ * @type {String} 
+ */
 var data_string = "";
 
 
-
-// Use connect method to connect to the server
+/**
+ * MongoDB接続
+ * @param {Object} 
+ */
 client.connect(function (err) {
-    assert.equal(null, err);
     console.log("Connected correctly to server");
-
-    const db = client.db(dbName);
-
-    // Get the documents collection
+    const db = client.db(DB_NAME);
     collection = db.collection('user');
 });
 
-function asyncFunc(index) {
+/**
+ * MongoDB内検索処理
+ * @param {Number} 1:レイアウトデータ 2:ガスデータ
+ * @returns {Object}
+ */
+function searchData(index) {
     return new Promise((resolve) => {
-        // ...何かしらの時間がかかる処理...
-        let result;
+        let data = collection.find({ "index": index });
 
-        let col = collection.find({ "index": index });
-        col.sort({ time: -1 }).toArray(function (err, docs) {
-            //collection.find({}).sort({ time: -1 }).toArray(function (err, docs) {
-            console.log("はいった");
+        data.sort({ time: -1 }).toArray(function (err, docs) {
             for (let doc of docs) {
-                result = doc;
+                var result = doc;
                 break;
             }
-            //value = docs;
             resolve(result);
         });
-
-        //process.on('unhandledRejection', console.dir)
-
     });
-
 }
 
-
-// http.createServerがrequestされたら、(イベントハンドラ)
+/**
+ * webサーバーの各処理
+ * @param {Object} 
+ * @param {Object} 
+ */
 server.on('request', function (req, res) {
-    // Responseオブジェクトを作成し、その中に必要な処理を書いていき、条件によって対応させる
     var Response = {
-        "renderHTML": function () {
+        /**
+        * HTMLファイルの取得
+        */
+        "getHTML": function () {
             var template = fs.readFile('./template/index.html', 'utf-8', function (err, data) {
-                // HTTPレスポンスヘッダを出力する
                 res.writeHead(200, {
                     'content-Type': 'text/html'
                 });
-
-                // HTTPレスポンスボディを出力する
                 res.write(data);
                 res.end();
-
             });
 
         },
+        /**
+        * Three.jsライブラリの取得
+        */
         "getThree": function () {
             var template = fs.readFile('./js/three.js-master/build/three.min.js', 'utf-8', function (err, data) {
-                // HTTPレスポンスヘッダを出力する
                 res.writeHead(200, {
                     'content-Type': 'text/javascript'
                 });
-
-                // HTTPレスポンスボディを出力する
                 res.write(data);
                 res.end();
             });
         },
+        /**
+        * 回転のためのライブラリの取得
+        */
         "getOrbit": function () {
             var template = fs.readFile('./js/OrbitControls.js', 'utf-8', function (err, data) {
-                // HTTPレスポンスヘッダを出力する
                 res.writeHead(200, {
                     'content-Type': 'text/javascript'
                 });
-
-                // HTTPレスポンスボディを出力する
                 res.write(data);
                 res.end();
             });
         },
+        /**
+        * リアルタイムグラフのためのライブラリの取得
+        */
         "getChartStreaming": function () {
             var template = fs.readFile('./js/chartjs-plugin-streaming.js', 'utf-8', function (err, data) {
-                // HTTPレスポンスヘッダを出力する
                 res.writeHead(200, {
                     'content-Type': 'text/javascript'
                 });
-
-                // HTTPレスポンスボディを出力する
                 res.write(data);
                 res.end();
             });
         },
+        /**
+        * 日付処理のためのライブラリの取得
+        */
         "getMoment": function () {
             var template = fs.readFile('./js/node_modules/moment/moment.js', 'utf-8', function (err, data) {
-                // HTTPレスポンスヘッダを出力する
                 res.writeHead(200, {
                     'content-Type': 'text/javascript'
                 });
-
-                // HTTPレスポンスボディを出力する
                 res.write(data);
                 res.end();
             });
         },
+        /**
+        * cssファイルの取得
+        */
         "getCSS": function () {
             var template = fs.readFile('./css/index.css', 'utf-8', function (err, data) {
-                // HTTPレスポンスヘッダを出力する
                 res.writeHead(200, {
                     'content-Type': 'text/css'
                 });
-
-                // HTTPレスポンスボディを出力する
                 res.write(data);
                 res.end();
             });
         },
+        /**
+        * レイアウトデータ取得
+        */
         "getValue": async function () {
+            let value = await searchData(1);
 
-            let value = await asyncFunc(1);
-            //client.close();
-
-            // HTTPレスポンスヘッダを出力する
             res.writeHead(200, {
                 'content-Type': 'text/html',
                 'Access-Control-Allow-Origin': '*'
             });
-
-            console.log("レスポンス");
-            //console.log(result);
-
-            console.log(value);
-            // HTTPレスポンスボディを出力する
             res.write(JSON.stringify(value));
             res.end();
         },
+        /**
+        * ガスデータ取得
+        */
         "getGas": async function () {
+            let value = await searchData(2);
 
-            let value = await asyncFunc(2);
-            //client.close();
-
-            // HTTPレスポンスヘッダを出力する
             res.writeHead(200, {
                 'content-Type': 'text/html',
                 'Access-Control-Allow-Origin': '*'
             });
-
-            console.log("レスポンス");
-            //console.log(result);
-
-            console.log(value);
-            // HTTPレスポンスボディを出力する
             res.write(JSON.stringify(value));
             res.end();
         },
+        /**
+        * レイアウトデータを登録
+        */
         "postData": function () {
             req.on('data', function (data) {
-                data_string += data.toString('utf-8', 0, data.length); //バッファをstringに逐一変換
+                /**
+                * バッファをstringに逐一変換
+                */
+                data_string += data.toString('utf-8', 0, data.length);
             });
             req.on('end', function () {
-                var data_json = JSON.parse(data_string); //stringをjsonに変換
-                //console.log(data_json.rot);
+                var data_json = JSON.parse(data_string);
 
-
-                //jsonにタイムスタンプを追加(あとで最新の物を検索して取り出せるように)
+                /**
+                * 最新データを検索するためにタイムスタンプを追加
+                * @type {Object} 
+                */
                 var time = new Date().getTime();
                 data_json["time"] = time;
-                console.log(data_json.time);
 
-                //index:1はレイアウト
+                /**
+                * index:1→レイアウト
+                */
                 data_json["index"] = 1;
 
-                //データをDBに保存
                 collection.insertOne(data_json);
                 console.log('json data insert DB');
             });
         },
+        /**
+        * ガスデータを登録
+        */
         "postGas": function () {
             req.on('data', function (data) {
-                data_string += data.toString('utf-8', 0, data.length); //バッファをstringに逐一変換
+                data_string += data.toString('utf-8', 0, data.length);
             });
             req.on('end', function () {
-                var data_json = JSON.parse(data_string); //stringをjsonに変換
-                //console.log(data_json.rot);
+                var data_json = JSON.parse(data_string);
 
-
-                //jsonにタイムスタンプを追加(あとで最新の物を検索して取り出せるように)
                 var time = new Date().getTime();
                 data_json["time"] = time;
                 console.log(data_json.time);
 
-                //index:2はgas
+                /**
+                * index:2→ガス
+                */
                 data_json["index"] = 2;
 
-                //データをDBに保存
                 collection.insertOne(data_json);
                 console.log('json data insert DB');
             });
         }
     };
-    // urlのpathをuriに代入
+    /**
+    * urlのpathをuriに変換
+    * @type {Object} 
+    */
     var uri = url.parse(req.url).pathname;
-    console.log(uri);
 
-
-    // URIで行う処理を分岐させる
+    /**
+    * uriで処理を分岐させる
+    */
     if (uri === "/") {
-        // URLが「IPアドレス/:1234/」の場合、"renderHTML"の処理を行う
-        Response["renderHTML"]();
+        Response["getHTML"]();
         return;
     } else if (uri === "/js/three.js-master/build/three.min.js") {
-        // URLが「IPアドレス/:1234/js/three.js-master/build/three.min.js」の場合、"getThree"の処理を行う
         Response["getThree"]();
         return;
     } else if (uri === "/js/OrbitControls.js") {
-        // URLが「IPアドレス/:1234/js/OrbitControls.js」の場合、"getOrbit"の処理を行う
         Response["getOrbit"]();
         return;
     } else if (uri === "/js/chartjs-plugin-streaming.js") {
-        // URLが「IPアドレス/:1234/js/chartjs-plugin-streaming.js」の場合、"getChartStreqming"の処理を行う
         Response["getChartStreaming"]();
         return;
     } else if (uri === "/js/node_modules/moment/moment.js") {
-        // URLが「IPアドレス/:1234/js/node_modules/moment/moment.js」の場合、"getMoment"の処理を行う
         Response["getMoment"]();
         return;
     } else if (uri === "/css/index.css") {
         Response["getCSS"]();
         return;
     } else if (uri === "/get_value") {
-        // URLが「IPアドレス/:1234/get_value」の場合、"getThree"の処理を行う
         Response["getValue"]();
         return;
     } else if (uri === "/get_gas") {
-        // URLが「IPアドレス/:1234/get_value」の場合、"getThree"の処理を行う
         Response["getGas"]();
         return;
     } else if (uri === "/post_data") {
-        // URLが「IPアドレス/:1234/post_data」の場合、"postData"の処理を行う
         Response["postData"]();
     } else if (uri === "/post_gas") {
-        // URLが「IPアドレス/:1234/post_gas」の場合、"postData"の処理を行う
         Response["postGas"]();
     };
 });
 
-// 指定されたポート(1234)でコネクションの受け入れを開始する
+
 server.listen(1234);
 console.log('Server running ');
